2eedac7ace5c5fa8d618afb09101cc7e19a861d3
AKerbel <evilkant@rambler.ru>
2015-06-13 19:40:21 +0300

    fixes in paginator

--- model/im_invoice_list.php
+++ model/im_invoice_list.php
@@ -4,41 +4,29 @@ if (!defined("WHMCS"))
 
 class im_invoice_list {
 	
-	public $page;
+	public $mainurl = '/whmcs_oss/admin/addonmodules.php?module=InvoiceManager';
+	public $page = 1;
 	public $perpage;
 	public $maxpage;
-	public $order;
+	public $order = 'id';
+	public $sort = 'DESC';
 	public $invoices = array();
 	public $tablehead;
 	public $paginator;
 	
 	public function __construct($perpage){
 		$this->perpage = $perpage;
-		
-		$page = $_GET['page'];
-		if ($page != NULL)
-			$this->page = $page;
-		else 
-			$this->page = 1;
-		
-		$order = $_GET['order'];
-		if ($order != NULL)
-			$this->order = $order;
-		else 
-			$this->order = 'id';
-		
-		$sort = $_GET['sort'];
-		if ($sort != NULL)
-			$this->sort = $sort;
-		else 
-			$this->sort = 'DESC';
+	
+		if ($_GET['page'] != NULL) $this->page = $_GET['page'];
+		if ($_GET['order'] != NULL) $this->order = $_GET['order'];	
+		if ($_GET['sort'] != NULL) $this->sort = $_GET['sort'];
 		
 		$this->createPaginator();
 		$result = full_query("
 			SELECT *
 			FROM tblinvoices
 			ORDER BY ".$this->order." ".$this->sort."
-			LIMIT ".($this->page-1).", $perpage
+			LIMIT ".(($this->page-1)*$this->perpage).", $perpage
 		");
 		
 		while ($invoice = mysql_fetch_assoc($result)){
@@ -54,24 +42,24 @@ class im_invoice_list {
 		");
 		$count = mysql_fetch_assoc($result);
 		$this->maxpage = floor($count['count']/$this->perpage);
-		
+		if (ceil($count['count']/$this->perpage) != $this->maxpage) $this->maxpage += 1;
 		$res = '';
 		if ($this->page>1)
-			$res .= '<a href="/admin/addonmodules.php?module=InvoiceManager&page='.($this->page-1).'"><< </a>';
+			$res .= '<a href="'.$this->getUrl(array('page' => ($this->page-1))).'"><< </a>';
 		if ($this->page-4 > 1)
 			$res .= '.. ';
 		for ($i = $this->page-4; $i <= $this->page+4; $i++) {
 			if (($i>0) and ($i<=$this->maxpage)){
 				if ($i!= $this->page) 
-					$res .= '<a href="/admin/addonmodules.php?module=InvoiceManager&page='.$i.'">'.$i.' </a>';
+					$res .= '<a href="'.$this->getUrl(array('page'=>$i)).'">'.$i.' </a>';
 				else
 					$res .= '<b>'.$i.' </b>';
 			}
 		}
 		if ($this->page+4 < $this->maxpage)
 			$res .= '.. ';
-		if ($page<$this->maxpage)
-			$res .= '<a href="/admin/addonmodules.php?module=InvoiceManager&page='.($this->page+1).'">>> </a>';
+		if ($this->page<$this->maxpage)
+			$res .= '<a href="'.$this->getUrl(array('page' => ($this->page+1))).'">>> </a>';
 		
 		$this->paginator = $res;
 	}
@@ -85,7 +73,7 @@ class im_invoice_list {
 		foreach ($newdata as $k=>$v){
 			$data[$k] = $v;
 		}
-		$url = '/whmcs_oss/admin/addonmodules.php?module=InvoiceManager';
+		$url = $this->mainurl;
 		foreach ($data as $k=>$v){
 			$url .= '&'.$k.'='.$v;
 		}
