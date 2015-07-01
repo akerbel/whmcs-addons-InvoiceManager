3c3b7e5b4e4e8eaafbcd06f4a8609e5411eb05a7
sverchkov <sverchkov@agava.com>
2015-07-01 18:27:00 +0300

    Fix proplem with invoicenum format in db

--- hooks.php
+++ hooks.php
@@ -3,7 +3,7 @@ if (!defined("WHMCS"))
     die("This file cannot be accessed directly");
 
 function hook_invoice_manager_createinvoicenum($vars){
-	$max = mysql_fetch_assoc(full_query('SELECT max(invoicenum) AS max FROM tblinvoices'));
+	$max = mysql_fetch_assoc(full_query('SELECT MAX(CAST(invoicenum AS INT)) AS max FROM tblinvoices'));
 	if ($max == null) $invoicenum = 1;
 	else $invoicenum = $max['max']+1;
 	update_query('tblinvoices', array('invoicenum' => $invoicenum), array('id' => $vars['invoiceid']));
