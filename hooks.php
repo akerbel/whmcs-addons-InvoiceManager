6abaa575abadeda3c247bd38f32db46d2d2d1e0b
sverchkov <sverchkov@agava.com>
2015-07-13 21:00:59 +0300

    Added check invoice total in hook

--- hooks.php
+++ hooks.php
@@ -8,20 +8,23 @@ function hook_invoice_manager_createinvoicenum($vars){
 	else $invoicenum = $max['max']+1;
 	update_query('tblinvoices', array('invoicenum' => $invoicenum), array('id' => $vars['invoiceid']));*/
 	
-	$invoice = full_query('SELECT invoicenum FROM tblinvoices');
-	$invoices = array();
-	while ($inv = mysql_fetch_assoc($invoice)){
-		if (($inv['invoicenum']) and ($inv['invoicenum'] != '')) $invoices[] = (int)$inv['invoicenum'];
+	$total = mysql_fetch_assoc(select_query('tblinvoices', 'total', array('id' => $vars['invoiceid'])));
+	if ($total['total'] != 0){ 
+		$invoice = full_query('SELECT invoicenum FROM tblinvoices');
+		$invoices = array();
+		while ($inv = mysql_fetch_assoc($invoice)){
+			if (($inv['invoicenum']) and ($inv['invoicenum'] != '')) $invoices[] = (int)$inv['invoicenum'];
+		}
+		sort($invoices);
+		$max = array_pop($invoices);
+		$newinvoicenum = (int)$max+1;
+		
+		$digits_data = mysql_fetch_assoc(select_query('tbladdonmodules', 'value', array('module' => 'InvoiceManager', 'setting' => 'NumberOfDigits')));
+		if ($digits_data) $digits = (int)$digits_data['value'];
+		else $digits = 0;
+		$newinvoicenum_str = str_pad((string)$newinvoicenum, $digits, "0", STR_PAD_LEFT);
+		update_query('tblinvoices', array('invoicenum' => $newinvoicenum_str), array('id' => $vars['invoiceid']));
 	}
-	sort($invoices);
-	$max = array_pop($invoices);
-	$newinvoicenum = (int)$max+1;
-	
-	$digits_data = mysql_fetch_assoc(select_query('tbladdonmodules', 'value', array('module' => 'InvoiceManager', 'setting' => 'NumberOfDigits')));
-	if ($digits_data) $digits = (int)$digits_data['value'];
-	else $digits = 0;
-	$newinvoicenum_str = str_pad((string)$newinvoicenum, $digits, "0", STR_PAD_LEFT);
-	update_query('tblinvoices', array('invoicenum' => $newinvoicenum_str), array('id' => $vars['invoiceid']));
 }
 
 add_hook('InvoicePaid', 555, 'hook_invoice_manager_createinvoicenum');
